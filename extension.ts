44e1d5440c53c22118ef2eb4c66ec8a4bd26633f
James Aprosail <aprosail@outlook.com>
2024-07-04 08:26:37 +0800

    client basic frame

--- extension.ts
+++ extension.ts
@@ -1,5 +1,29 @@
-import {ExtensionContext} from "vscode"
+import {ExtensionContext, workspace} from "vscode"
+import {
+  LanguageClient,
+  LanguageClientOptions,
+  ServerOptions,
+  TransportKind,
+} from "vscode-languageclient/node"
 
-export function activate(_context: ExtensionContext) {}
+let client: LanguageClient | null = null
 
-export function deactivate() {}
+const vDocHandler = new Map<string, string>()
+
+export function activate(_context: ExtensionContext) {
+  workspace.registerTextDocumentContentProvider("code-in-doc", {
+    provideTextDocumentContent: (uri) => vDocHandler.get(uri.toString(true)),
+  })
+
+  const serverOpts: ServerOptions = {command: "", transport: TransportKind.ipc}
+  const clientOpts: LanguageClientOptions = {
+    documentSelector: [{scheme: "file", language: "markdown"}],
+  }
+
+  client = new LanguageClient("code-in-doc", serverOpts, clientOpts)
+  client?.start()
+}
+
+export function deactivate() {
+  client?.stop()
+}
